f9edad93f50642efd91ff17549a05a58812a26ec
AndreyPokotilo <102250174+AndreyPokotilo@users.noreply.github.com>
2023-03-11 19:55:57 +0100

    adedd all tasks

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const bodyRef = document.querySelector('body');
 
+btnStart.disabled = false;
+btnStop.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timerId = null;
+
+btnStart.addEventListener('click', onClickBtnStart);
+btnStop.addEventListener('click', onClickBtnStop);
+
+function onClickBtnStart() {
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+  timerId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  console.log("timerId:", timerId)
+  
+}
+
+function onClickBtnStop() {
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+  clearInterval(timerId);
+}
