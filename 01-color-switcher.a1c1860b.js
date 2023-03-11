c74f8677a4e749d82f65aa57d11589c12020fc3a
AndreyPokotilo <102250174+AndreyPokotilo@users.noreply.github.com>
2023-03-11 18:56:57 +0000

    Deploying to gh-pages from @ AndreyPokotilo/goit-js-hw-09@f9edad93f50642efd91ff17549a05a58812a26ec 🚀

---
+++ 01-color-switcher.a1c1860b.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("button[data-start]"),t=document.querySelector("button[data-stop]"),d=document.querySelector("body");e.disabled=!1,t.disabled=!0;let o=null;e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,o=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),console.log("timerId:",o)})),t.addEventListener("click",(function(){e.disabled=!1,t.disabled=!0,clearInterval(o)}));
+//# sourceMappingURL=01-color-switcher.a1c1860b.js.map
