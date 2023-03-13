c527c944062034bba68d7433299683714aeeb636
AndreyPokotilo <102250174+AndreyPokotilo@users.noreply.github.com>
2023-03-13 19:49:24 +0100

    fixed last task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,6 +1,7 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const formRef = document.querySelector('.form');
+
 formRef.addEventListener('submit', onSubmitForm);
 
 function onSubmitForm(e) {
@@ -8,6 +9,7 @@ function onSubmitForm(e) {
   let delay = Number(e.target.delay.value);
   const step = Number(e.target.step.value);
   const amount = Number(e.target.amount.value);
+  console.log({delay, step, amount});
 
   for (let position = 1; position <= amount; position += 1) {
     createPromise(position, delay)
@@ -21,9 +23,13 @@ function onSubmitForm(e) {
           Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
         }, delay);
       });
-
+    console.log("delay:", delay);
     delay += step;
   }
+
+  e.currentTarget.elements.step.value = '';
+  e.currentTarget.elements.delay.value = '';
+  e.currentTarget.elements.amount.value = '';
 }
 
 function createPromise(position, delay) {
